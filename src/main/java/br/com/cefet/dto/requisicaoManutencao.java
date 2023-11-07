d62402f84189e2165baf5791565fabd703868468
Manoel-Mieiro <mjlmieiro@gmail.com>
2023-11-07 17:17:33 -0300

    Adicionados getters e setter à requisição de manutenção

--- src/main/java/br/com/cefet/dto/requisicaoManutencao.java
+++ src/main/java/br/com/cefet/dto/requisicaoManutencao.java
@@ -9,8 +9,8 @@ import jakarta.validation.constraints.NotNull;
 
 public class requisicaoManutencao {
 	private Veiculo veiculo;
-    @NotNull
-    private int veiculoId; 
+	@NotNull
+	private int veiculoId;
 	@NotNull
 	private Estacao estacao;
 	@NotNull
@@ -20,43 +20,84 @@ public class requisicaoManutencao {
 	@NotNull
 	private Date dataSaida;
 
-	
-public Manutencao toManutencao() {
+	public Veiculo getVeiculo() {
+		return veiculo;
+	}
+
+	public void setVeiculo(Veiculo veiculo) {
+		this.veiculo = veiculo;
+	}
+
+	public int getVeiculoId() {
+		return veiculoId;
+	}
+
+	public void setVeiculoId(int veiculoId) {
+		this.veiculoId = veiculoId;
+	}
+
+	public Estacao getEstacao() {
+		return estacao;
+	}
+
+	public void setEstacao(Estacao estacao) {
+		this.estacao = estacao;
+	}
+
+	public int getEstacaoId() {
+		return estacaoId;
+	}
+
+	public void setEstacaoId(int estacaoId) {
+		this.estacaoId = estacaoId;
+	}
+
+	public Date getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(Date dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public Date getDataSaida() {
+		return dataSaida;
+	}
+
+	public void setDataSaida(Date dataSaida) {
+		this.dataSaida = dataSaida;
+	}
+
+	public Manutencao toManutencao() {
 		Manutencao manutencao = new Manutencao();
 		manutencao.setVeiculo(this.veiculo);
 		manutencao.setEstacao(this.estacao);
 		manutencao.setDataEntrada(this.dataEntrada);
 		manutencao.setDataSaida(this.dataSaida);
-		
+
 		return manutencao;
-	} 
-
-public Manutencao toManutencao(Manutencao manutencao) {
-	manutencao.setVeiculo		(this.veiculo);
-	manutencao.setEstacao		(this.estacao);
-	manutencao.setDataEntrada	(this.dataEntrada);
-	manutencao.setDataSaida		(this.dataSaida);
-	
-	return manutencao;
-} 
-
-public void fromManutencao(Manutencao manutencao) {
-	this.veiculo  = manutencao.getVeiculo();
-	this.estacao = manutencao.getEstacao();
-	this.dataEntrada = manutencao.getDataEntrada();
-	this.dataSaida= manutencao.getDataSaida();
-}
-
-@Override
-public String toString() {
-	return "requisicaoManutencao [veiculo=" + veiculo + ", estacao=" + estacao + ", dataEntrada=" + dataEntrada
-			+ ", dataSaida=" + dataSaida + "]";
-}
-
-
-	
-	
-	
-	
-	
 	}
+
+	public Manutencao toManutencao(Manutencao manutencao) {
+		manutencao.setVeiculo(this.veiculo);
+		manutencao.setEstacao(this.estacao);
+		manutencao.setDataEntrada(this.dataEntrada);
+		manutencao.setDataSaida(this.dataSaida);
+
+		return manutencao;
+	}
+
+	public void fromManutencao(Manutencao manutencao) {
+		this.veiculo = manutencao.getVeiculo();
+		this.estacao = manutencao.getEstacao();
+		this.dataEntrada = manutencao.getDataEntrada();
+		this.dataSaida = manutencao.getDataSaida();
+	}
+
+	@Override
+	public String toString() {
+		return "requisicaoManutencao [veiculo=" + veiculo + ", estacao=" + estacao + ", dataEntrada=" + dataEntrada
+				+ ", dataSaida=" + dataSaida + "]";
+	}
+
+}
