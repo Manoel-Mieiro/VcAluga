9dbbfafbb96f344921908772a1d52503422b50cf
Manoel Mieiro <manoeljorgemieiro@gmail.com>
2023-12-01 14:48:45 -0300

    Continuação da NF

--- src/main/java/br/com/cefet/model/NF.java
+++ src/main/java/br/com/cefet/model/NF.java
@@ -174,4 +174,8 @@ public class NF {
 	        return TimeUnit.DAYS.convert(timeDiff, TimeUnit.MILLISECONDS);
 	    }
 	
+	  private float arredondarAliquota(float valor) {
+	        return Math.round(valor * 100.0) / 100.0;
+	    }
+	  
 }
